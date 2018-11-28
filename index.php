207eeff71b9116aee75af65afe8f0e2f41d83751
Heiko John <healjo@hotmail.com>
2018-11-28 14:13:39 +0100

    No direct html-code in index.php and starting with step 2

--- index.php
+++ index.php
@@ -10,122 +10,210 @@ for ($i=0; $i < 3; $i++) {
 }
 
 $prename = $surname = $street = $city = $zip = $phone = $email = "";
+$prename_err = $surname_err = $street_err = $city_err = $zip_err = $phone_err = $email_err = false;
+$prename_err_msg = $surname_err_msg = $street_err_msg = $city_err_msg = $zip_err_msg = $phone_err_msg = $email_err_msg = false;
+$prename_corr = $surname_corr = $street_corr = $city_corr = $zip_corr = $phone_corr = $email_corr = false;
 
 $step = "";
 
 //Regular Expressions
-$allowed_chars = "/[^A-Za-z0-9- ]/";
+$allowed_chars = "/[^a-zA-Z0-9- ]/";
+$prename_chars = $surname_chars = $city_chars = "/^[a-zA-ZäöüÄÖÜ \-]*$/";
+$street_chars = "/[a-zA-ZäöüÄÖÜ \.]+ [0-9]+[a-zA-Z]?/";
+$plz_chars = "/^[0-9]{5}$/";
+$phone_chars = "/^[0-9 \(\)\/\-\+]*$/";
+$email_chars = "/\A[^@]+@[^@]+\z/";
+
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	
-	$wish[0] = $_POST["wish01"];
-	$wish[1] = $_POST["wish02"];
-	$wish[2] = $_POST["wish03"];
 	
-	$step = $_POST["step01"];
+	var_dump($_POST);
 	
-	if (isset($_POST["step01"]) && $step == "step01") {
+	if (isset($_POST["step01"])) {
 		
-		if (!empty($wish[0])) {
+		$wish[0] = $_POST["wish01"];
+		$wish[1] = $_POST["wish02"];
+		$wish[2] = $_POST["wish03"];
+		
+		$step = $_POST["step01"];
+		
+		if ($step == "step01") {
 			
-			if (preg_match($allowed_chars, $wish[0])) {
+			if (!empty($wish[0])) {
+			
+				if (preg_match($allowed_chars, $wish[0])) {
+					$wish_err[0] = true;
+					$wish_err_msg[0] = "Special characters used.";
+				} else {
+					$wish_corr[0] = true;
+				}
+			} else {
 				$wish_err[0] = true;
-				$wish_err_msg[0] = "Special characters used.";
-				echo $wish_err_msg[0];
-			} else {
-				echo $wish[0];
-				$wish_corr[0] = true;
+				$wish_err_msg[0] = "No entry.";
 			}
-		} else {
-			$wish_err[0] = true;
-			$wish_err_msg[0] = "No entry.";
-			echo $wish_err_msg[0];
-		}
-		
-		if (!empty($wish[1])) {
 			
-			if (preg_match($allowed_chars, $wish[1])) {
+			if (!empty($wish[1])) {
+				
+				if (preg_match($allowed_chars, $wish[1])) {
+					$wish_err[1] = true;
+					$wish_err_msg[1] = "Special characters used.";
+				} else {
+					$wish_corr[1] = true;
+				}
+			} else {
 				$wish_err[1] = true;
-				$wish_err_msg[1] = "Special characters used.";
-				echo $wish_err_msg[1];
-			} else {
-				echo $wish[1];
-				$wish_corr[1] = true;
+				$wish_err_msg[1] = "No entry.";
 			}
-		} else {
-			$wish_err[1] = true;
-			$wish_err_msg[1] = "No entry.";
-			echo $wish_err_msg[1];
-		}
-		
-		if (!empty($wish[2])) {
 			
-			if (preg_match($allowed_chars, $wish[2])) {
-				$wish_err[2] = true;
-				$wish_err_msg[2] = "Special characters used.";
-				echo $wish_err_msg[2];
+			if (!empty($wish[2])) {
+				
+				if (preg_match($allowed_chars, $wish[2])) {
+					$wish_err[2] = true;
+					$wish_err_msg[2] = "Special characters used.";
+				} else {
+					$wish_corr[2] = true;
+				}
 			} else {
-				echo $wish[2];
-				$wish_corr[2] = true;
+				$wish_err[2] = true;
+				$wish_err_msg[2] = "No entry.";
 			}
-		} else {
-			$wish_err[2] = true;
-			$wish_err_msg[2] = "No entry.";
-			echo $wish_err_msg[2];
 		}
 	}
 
-	if ($wish_corr[0]==true || $wish_corr[1]==true || $wish_corr[2]==true) {
+	
+	if (isset($_POST["step02"])) {
 		
-		echo "<form method=\"post\" action=\" \">";
+		$prename = $_POST["prename"];
+		$name = $_POST["name"];
+		$street = $_POST["street"];
+		$city = $_POST["city"];
+		$zip = $_POST["zip"];
+		$phone = $_POST["phone"];
+		$email = $_POST["email"];
+		
+		$step = $_POST["step02"];
+		
+		if ($step == "step02") {
+			
+			if (!empty($prename)) {
+			
+				if (preg_match($prename_chars, $prename)) {
+					$prename_err = true;
+					$prename_err_msg = "Unusual characters used.";
+				} else {
+					$prename_corr = true;
+				}
+			} else {
+				$prename_err = true;
+				$prename_err_msg = "Prename required.";
+			}
+			
+			if (!empty($surname)) {
+			
+				if (preg_match($surname_chars, $surname)) {
+					$surname_err = true;
+					$surname_err_msg = "Unusual characters used.";
+				} else {
+					$surname_corr = true;
+				}
+			} else {
+				$surname_err = true;
+				$surname_err_msg = "Surname required.";
+			}
+			
+			if (!empty($street)) {
+			
+				if (preg_match($street_chars, $street)) {
+					$street_err = true;
+					$street_err_msg = "Unusual characters used.";
+				} else {
+					$street_corr = true;
+				}
+			} else {
+				$street_err = true;
+				$street_err_msg = "Street required.";
+			}
+			
+			var_dump($_POST);
+		}
+	}
+	
+	if ($wish_corr[0] == false && $wish_corr[1] == false && $wish_corr[2] == false) {
+		
+		echo "<form method=\"post\" action=\"index.php\">";
+		echo "<div class=\"form-group\">";
+		echo "<label for=\"wish01\">Wish No. 1:</label>";
+		echo "<input type=\"text\" id=\"wish01\" name=\"wish01\" value=\"{$wish[0]}\" class=\"form-control\" />";
+		echo "<div class=\"error\">{$wish_err_msg[0]}</div>";
+		echo "<label for=\"wish02\">Wish No. 2:</label>";
+		echo "<input type=\"text\" id=\"wish01\" name=\"wish02\" value=\"{$wish[1]}\" class=\"form-control\" />";
+		echo "<div class=\"error\">{$wish_err_msg[1]}</div>";
+		echo "<label for=\"wish03\">Wish No. 3:</label>";
+		echo "<input type=\"text\" id=\"wish03\" name=\"wish03\" value=\"{$wish[2]}\" class=\"form-control\" />";
+		echo "<div class=\"error\">{$wish_err_msg[2]}</div>";
+		echo "<input type=\"hidden\" name=\"step01\" value=\"step01\" />";
+		echo "</div>";
+		echo "<button type=\"submit\" class=\"btn btn-primary\">Submit</button>";
+		echo "</form>";
+		
+	} 
+	
+	if ($wish_corr[0] == true || $wish_corr[1] == true || $wish_corr[2] == true) {
+		
+		echo "<form method=\"post\" action=\"index.php\">";
 		echo "<div class=\"form-group\">";
 		echo "<label for=\"wish01\">Wish No. 1:</label>";
 		echo "<input type=\"text\" id=\"wish01\" name=\"wish01\" value=\"{$wish[0]}\" class=\"form-control\" readonly />";
+		echo "<div class=\"error\">{$wish_err_msg[0]}</div>";
 		echo "<label for=\"wish02\">Wish No. 2:</label>";
 		echo "<input type=\"text\" id=\"wish01\" name=\"wish02\" value=\"{$wish[1]}\" class=\"form-control\" readonly />";
+		echo "<div class=\"error\">{$wish_err_msg[1]}</div>";
 		echo "<label for=\"wish03\">Wish No. 3:</label>";
 		echo "<input type=\"text\" id=\"wish03\" name=\"wish03\" value=\"{$wish[2]}\" class=\"form-control\" readonly />";
+		echo "<div class=\"error\">{$wish_err_msg[2]}</div>";
+		echo "</div>";
+		echo "<div class=\"form-group\">";
+		echo "<label for=\"prename\">Prename:</label>";
+		echo "<input type=\"text\" id=\"prename\" name=\"prename\" class=\"form-control\" />";
+		echo "<label for=\"name\">Name:</label>";
+		echo "<input type=\"text\" id=\"name\" name=\"name\" class=\"form-control\" />";
+		echo "<label for=\"street\">Street:</label>";
+		echo "<input type=\"text\" id=\"street\" name=\"street\" class=\"form-control\" />";
+		echo "<label for=\"city\">City:</label>";
+		echo "<input type=\"text\" id=\"city\" name=\"city\" class=\"form-control\" />";
+		echo "<label for=\"zip\">Zip:</label>";
+		echo "<input type=\"text\" id=\"zip\" name=\"zip\" class=\"form-control\" />";
+		echo "<label for=\"phone\">Phone:</label>";
+		echo "<input type=\"text\" id=\"phone\" name=\"phone\" class=\"form-control\" />";
+		echo "<label for=\"email\">Email:</label>";
+		echo "<input type=\"text\" id=\"email\" name=\"email\" class=\"form-control\" />";
+		echo "<input type=\"hidden\" name=\"step02\" value=\"step02\" />";
+		echo "</div>";
+		echo "<button type=\"submit\" class=\"btn btn-primary\">Contact Us</button>";
 		echo "</form>";
 		
 	}
-
+	
+} else {
+	
+	echo "<form method=\"post\" action=\"index.php\">";
+	echo "<div class=\"form-group\">";
+	echo "<label for=\"wish01\">Wish No. 1:</label>";
+	echo "<input type=\"text\" id=\"wish01\" name=\"wish01\" value=\"{$wish[0]}\" class=\"form-control\" />";
+	echo "<div class=\"error\">{$wish_err_msg[0]}</div>";
+	echo "<label for=\"wish02\">Wish No. 2:</label>";
+	echo "<input type=\"text\" id=\"wish01\" name=\"wish02\" value=\"{$wish[1]}\" class=\"form-control\" />";
+	echo "<div class=\"error\">{$wish_err_msg[1]}</div>";
+	echo "<label for=\"wish03\">Wish No. 3:</label>";
+	echo "<input type=\"text\" id=\"wish03\" name=\"wish03\" value=\"{$wish[2]}\" class=\"form-control\" />";
+	echo "<div class=\"error\">{$wish_err_msg[2]}</div>";
+	echo "<input type=\"hidden\" name=\"step01\" value=\"step01\" />";
+	echo "</div>";
+	echo "<button type=\"submit\" class=\"btn btn-primary\">Submit</button>";
+	echo "</form>";
+	
 }
 
+require_once 'includes/footer.php';
 ?>
-
-
-
-			<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-				<div class="form-group">
-					<label for="wish01">Wish No. 1:</label>
-					<input type="text" id="wish01" name="wish01" class="form-control" />
-					<label for="wish02">Wish No. 2:</label>
-					<input type="text" id="wish02" name="wish02" class="form-control" />
-					<label for="wish03">Wish No. 3:</label>
-					<input type="text" id="wish03" name="wish03" class="form-control" />
-					<input type="hidden" name="step01" value="step01" />
-				</div>
-				<button type="submit" class="btn btn-primary">Submit</button>
-			</form>
-			<!--
-			<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-				<div class="form-group">
-					<label for="prename">Prename:</label>
-					<input type="text" id="prename" name="prename" class="form-control" />
-					<label for="name">Name:</label>
-					<input type="text" id="name" name="name" class="form-control" />
-					<label for="street">Street:</label>
-					<input type="text" id="street" name="street" class="form-control" />
-					<label for="city">City:</label>
-					<input type="text" id="city" name="city" class="form-control" />
-					<label for="zip">Zip-Code:</label>
-					<input type="text" id="zip" name="zip" class="form-control" />
-					<label for="phone">Phone:</label>
-					<input type="text" id="phone" name="phone" class="form-control" />
-					<label for="email">Email:</label>
-					<input type="text" id="email" name="email" class="form-control" />
-				</div>
-				<button type="submit" class="btn btn-primary">Submit</button>
-			</form>
-			-->
-<?php require_once 'includes/footer.php'; ?>
